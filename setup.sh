3f3ce77ae849f2d552d7b54148145aade18a4bcc
Arpit Gupta <grpit.github@gmail.com>
2021-10-29 03:35:00 +0530

    Initial setup files added

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Install ansible
+pip install ansible
+ansible-galaxy collection install kewlfft.aur
+
+# Run this manually and enter password when prompted
+# ansible-playbook -i ansible_hosts --ask-become-pass playbook.yml
+
