218bf9ffd5d92b57db034f37d3c495e3ff209ba1
Arpit Gupta <grpit.github@gmail.com>
2021-10-30 01:06:17 +0530

    Added yay for AUR Added K3D Added kubectl Some Golang changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# install base packages and git
+sudo pacman -S --needed git base-devel -y 
+
 # Install ansible
 pip install ansible
 ansible-galaxy collection install kewlfft.aur
@@ -5,3 +8,6 @@ ansible-galaxy collection install kewlfft.aur
 # Run this manually and enter password when prompted
 # ansible-playbook -i ansible_hosts --ask-become-pass playbook.yml
 
+# Configure git
+git config --global user.name "Arpit Gupta"
+git config --global user.email "grpit.github@gmail.com"
