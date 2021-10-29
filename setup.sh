4e49e66c45133be30ad649fbd95f71094093138e
Arpit Gupta <grpit.github@gmail.com>
2021-10-30 03:40:37 +0530

    Added Node and nvm.

--- setup.sh
+++ setup.sh
@@ -3,11 +3,18 @@ sudo pacman -S --needed git base-devel -y
 
 # Install ansible
 pip install ansible
+pip3 install pynvim
 ansible-galaxy collection install kewlfft.aur
 
 # Run this manually and enter password when prompted
-# ansible-playbook -i ansible_hosts --ask-become-pass playbook.yml
+ansible-playbook -i ansible_hosts --ask-become-pass playbook.yml
 
 # Configure git
 git config --global user.name "Arpit Gupta"
 git config --global user.email "grpit.github@gmail.com"
+
+# Source from new zsh file
+source ~/.zshrc
+
+# Install latest node and npm
+nvm install --lts
